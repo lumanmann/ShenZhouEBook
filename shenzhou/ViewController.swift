167b8564809a4bed8305774f68d26d8fcd162544
Your Name <you@example.com>
2018-02-02 15:20:06 +0800

    init upload

---
+++ shenzhou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shenzhou
+//
+//  Created by WY NG on 12/11/2017.
+//  Copyright © 2017 WYNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
